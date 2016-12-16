5ad1686e5320219b1034737a6b2f5c5cce72eb02
Edd Tsao <edd.tsao@carinacare.com>
2016-12-16 10:31:00 -0800

    Remove example code

--- app/App/index.jsx
+++ app/App/index.jsx
@@ -2,14 +2,11 @@
 
 import React from 'react'
 
-import TestComponent from '../TestComponent'
-
 export default class App extends React.Component {
   render () {
     return (
       <div>
         <p>Base Frontend Server has arrived.</p>
-        <TestComponent start={10} increment={1}/>
       </div>
     )
   }
